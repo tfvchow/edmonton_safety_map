9d52771a9f295c3201ad3eb51eebf2f3d6ee9e33
Vincent Tsz-Fai Chow <tfvchow@gmail.com>
2024-07-06 13:13:08 -0400

    Include heatmap

--- map.py
+++ map.py
@@ -1,9 +1,26 @@
+from pyproj import Transformer
+
 import folium
 import geopandas as gpd
+import pandas as pd
 
 shapefile_path = './shapefile/edmonton.shp'
 gdf = gpd.read_file(shapefile_path)
 gdf = gdf.set_crs('EPSG:4326', allow_override=True).to_crs('EPSG:4326')
+gdf["neighbourh"] = gdf["neighbourh"].astype(int)
+gdf.set_index("neighbourh", inplace=True)
+
+# Load CSV data
+data_path = "./data/edmonton_safety_data.csv"
+data = pd.read_csv(data_path)
+
+transformer = Transformer.from_crs(3857, 4326)
+data['coordinates'] = data.apply(lambda x: transformer.transform(x['x'], x['y']), axis=1)
+points = gpd.GeoDataFrame(data, geometry=gpd.points_from_xy(data.coordinates.str[1], data.coordinates.str[0]))
+
+# Spatial join - points to shapes
+joined = gpd.sjoin(points, gdf, how="inner", predicate="within")
+joined = joined.groupby('neighbourh')["Occurrence_Type_Group"].count()
 
 # Convert the GeoDataFrame to GeoJSON
 geojson_data = gdf.to_json()
@@ -15,11 +32,6 @@ combined_centroid = combined_shape.centroid
 # Note the coordinates have to be reversed
 m = folium.Map(location=[combined_centroid.y, combined_centroid.x], zoom_start=11)
 
-folium.GeoJson(
-    geojson_data,
-    name='geojson'
-).add_to(m)
-
 tooltip = folium.GeoJsonTooltip(
     fields=['descriptiv'],
     aliases=[''],
@@ -29,10 +41,24 @@ tooltip = folium.GeoJsonTooltip(
     ),
 )
 
+choropleth = folium.Choropleth(
+    geo_data=geojson_data,
+    data=joined,
+    key_on='id',
+    fill_color='RdYlGn_r',
+    use_jenks=True,
+)
+choropleth.add_to(m)
+choropleth.color_scale.width = 1200
+
 folium.GeoJson(
     geojson_data,
     name='geojson',
-    tooltip=tooltip
+    tooltip=tooltip,
+    style_function=lambda feature: {
+        'fillOpacity': 0.0,
+        'opacity': 0.0
+    }
 ).add_to(m)
 
 folium.LayerControl().add_to(m)
